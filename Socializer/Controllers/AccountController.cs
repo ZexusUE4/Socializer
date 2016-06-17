cb046cbb00348a3b0abed70e3d7513a11974dc3a
Mahmoud Aladdin <m.aladdin.gn@gmail.com>
2016-06-17 16:16:28 +0200

    Changed Home, Login and Register Views

--- Socializer/Controllers/AccountController.cs
+++ Socializer/Controllers/AccountController.cs
@@ -417,7 +417,7 @@ namespace Socializer.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
