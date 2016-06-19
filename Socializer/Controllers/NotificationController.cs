091f135e185d2584b9004df7ecca0b9c85d980c1
Mahmoud Aladdin <m.aladdin.gn@gmail.com>
2016-06-19 11:16:43 +0200

    Notification List

---
+++ Socializer/Controllers/NotificationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using Socializer.DAL;
+using Socializer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Socializer.Controllers
+{
+    [Authorize]
+    public class NotificationController : Controller
+    {
+        private SocializerContext db = new SocializerContext();
+
+        public ActionResult NotificationList()
+        {
+            SUser currentLogged = db.Users.Find(User.Identity.GetUserId());
+            List<Notification> notifs = currentLogged.Notifications.ToList();
+            return View(notifs);
+        }
+    }
+}
